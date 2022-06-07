0a132a5e8127101871f852319ccb7ad18ce6ad38
hubertnakielski <01153087@pw.edu.pl>
2022-06-07 14:58:26 +0200

    Add threads support

--- src/main/java/com/hnks/raytraycer/Main.java
+++ src/main/java/com/hnks/raytraycer/Main.java
@@ -1,18 +1,41 @@
 package com.hnks.raytraycer;
 
-import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.util.stream.StreamSupport;
+import javax.imageio.ImageIO;
 
 public class Main {
+
+    static final int THREADS_NUM = 10;
+
     public static void main(String[] args) {
+
         try {
 
             Raytracer raytracer = new RaytracerLoader(new File("scene.dat")).read();
-            raytracer.raytrace();
+            raytracer.generateStack();
+            Thread[] threads = new Thread[THREADS_NUM];
+
+            long start = System.nanoTime();
+
+            for (int i = 0; i < THREADS_NUM; i++) {
+                threads[i] = new Thread(raytracer);
+                threads[i].setName("Thread: " + i);
+                threads[i].start();
+
+                System.out.println("Running " + threads[i].getName());
+            }
+
+            for (int i = 0; i < THREADS_NUM; i++) {
+                threads[i].join();
+            }
 
             ImageIO.write(raytracer.getImage(), "png", new File("out.png"));
 
-        } catch (IOException ignored) {}
+            System.out.println("Execution time: " + (double)(System.nanoTime() - start)/ 10000000000L);
+
+        } catch (IOException | InterruptedException ignored) {
+        }
     }
 }
--- src/main/java/com/hnks/raytraycer/Raytracer.java
+++ src/main/java/com/hnks/raytraycer/Raytracer.java
@@ -5,21 +5,26 @@ import com.hnks.raytraycer.ray.Ray;
 import com.hnks.raytraycer.scene.Scene;
 import com.hnks.raytraycer.scene.camera.Camera;
 import com.hnks.raytraycer.util.ColorUtil;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
+@Builder
+@AllArgsConstructor
+public class Raytracer implements Runnable {
 
-@Builder @AllArgsConstructor
-public class Raytracer {
     private final int width, height;
-    private BufferedImage image;
+    private volatile BufferedImage image;
 
     private final Scene scene;
     private final Camera camera;
 
     private final int sampleCount;
+
+    private volatile Deque<int[]> dividedStack;
 
     public Raytracer(Scene scene, Camera camera, int sampleCount, int width, int height) {
         this.scene = scene;
@@ -28,6 +33,7 @@ public class Raytracer {
 
         this.width = width;
         this.height = height;
+        generateStack();
         createImage();
     }
 
@@ -54,16 +60,18 @@ public class Raytracer {
         }
 
         Color color = ColorUtil.linearVectorToColor(
-                sampleGather.scale(1.0 / sampleCount)
+            sampleGather.scale(1.0 / sampleCount)
         );
 
-        image.setRGB(x, y, color.getRGB());
+        synchronized (image) {
+            image.setRGB(x, y, color.getRGB());
+        }
     }
 
     public void raytrace(int left, int top, int width, int height) {
         for (int x = left; x < (left + width); x++) {
             for (int y = top; y < (top + height); y++) {
-                if (x >= width || y >= height) {
+                if (x >= this.width || y >= this.height) {
                     continue;
                 }
 
@@ -73,6 +81,30 @@ public class Raytracer {
     }
 
     public void raytrace() {
+        if (dividedStack.isEmpty()) {
+            generateStack();
+        }
         raytrace(0, 0, width, height);
     }
+
+    @Override
+    public void run() {
+        while (!dividedStack.isEmpty()) {
+            int[] startingPoint = dividedStack.pop();
+            int localWidth = Math.min(width - startingPoint[0], 64);
+            int localHeight = Math.min(height - startingPoint[1], 64);
+
+            raytrace(startingPoint[0], startingPoint[1], localWidth, localHeight);
+        }
+    }
+
+    public void generateStack() {
+        dividedStack = new ArrayDeque<>();
+        for (int x = 0; x < width; x = x + 64) {
+            for (int y = 0; y < height; y = y + 64) {
+                dividedStack.push(new int[]{x, y});
+            }
+        }
+    }
+
 }
